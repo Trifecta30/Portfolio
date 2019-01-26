4130f10ddf3d85f2204896748706c20de2c0fb64
Trifecta30 <36042260+Trifecta30@users.noreply.github.com>
2019-01-26 01:44:53 -0500

    Create Wat4dinner.js

---
+++ Projects/Intro to JS/Wat4dinner.js
@@ -0,0 +1,28 @@
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); 
+fill(207, 207, 207);
+noStroke();
+ellipse(150, 150, 150, 150);
+fill(255, 153, 153);
+ellipse(150, 150, 110, 110);
+stroke(161, 161, 161);
+strokeWeight(13);
+line(227, 228, 357, 239);
+strokeWeight(20);
+fill(209, 144, 144);
+ellipse (205, 230, 54, 35);
+strokeWeight(6);
+noStroke();
+fill(40, 153, 34);
+ellipse(151, 103, 12, 12);
+ellipse(167, 145, 12, 12);
+ellipse(189, 174, 12, 12);
+ellipse(136, 139, 12, 12);
+ellipse(182, 154, 12, 12);
+ellipse(182, 139, 12, 12);
+ellipse(109, 122, 12, 12);
+ellipse(172, 177, 12, 12);
+ellipse(154, 160, 12, 12);
+ellipse(105, 166, 12, 12);
+
